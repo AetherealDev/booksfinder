62146d410628aeee210a99c87db87f4a3d67afb4
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-06-21 18:04:01 -0400

    made genres responsive

--- assets/js/genres.js
+++ assets/js/genres.js
@@ -1,63 +1,85 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const genreItems = document.querySelectorAll('.genre-item');
-  
-    genreItems.forEach(function(item) {
-      const genreName = item.querySelector('span').textContent;
-      const bookContainer = item.querySelector('.books');
-  
-      axios.get('https://www.googleapis.com/books/v1/volumes?q=subject:' + genreName)
-        .then(function(response) {
-          const books = response.data.items;
-  
-          books.forEach(function(book) {
-            const title = book.volumeInfo.title;
-            const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Unknown Author';
-            const thumbnail = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'https://via.placeholder.com/100';
-  
-            const bookElement = document.createElement('div');
-            bookElement.classList.add('card', 'book-card');
-            bookElement.innerHTML = `
-              <img src="${thumbnail}" class="card-img-top" alt="Book Cover">
-              <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-                <p class="card-text">${authors}</p>
-              </div>
-            `;
-  
-            const favoritesButton = document.createElement('button');
-            favoritesButton.classList.add('button', 'is-primary', 'favorite-button');
-            favoritesButton.textContent = 'Favorite';
-            favoritesButton.addEventListener('click', function() {
-              favoriteBook(book);
-            });
-  
-            bookElement.appendChild(favoritesButton); // Append the button to the card element
-  
-            bookContainer.appendChild(bookElement);
-            bookContainer.appendChild(document.createElement('br'));
-          });
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    });
-  });
-  
+  const genreItems = document.querySelectorAll('.genre-item');
 
-  function favoriteBook(book) {
-    // Retrieve the existing favorites from local storage
-    const favorites = localStorage.getItem('favorites') || '[]';
-  
-    // Parse the favorites into an array
-    const favoritesArray = JSON.parse(favorites);
-  
-    // Add the book to the favorites array
-    favoritesArray.push(book);
-  
-    // Convert the favorites array back to a string
-    const updatedFavorites = JSON.stringify(favoritesArray);
-  
-    // Store the updated favorites in local storage
-    localStorage.setItem('favorites', updatedFavorites);
+  genreItems.forEach(function(item) {
+    const genreName = item.querySelector('span').textContent;
+    const bookContainer = item.querySelector('.books');
+
+    axios.get('https://www.googleapis.com/books/v1/volumes?q=subject:' + genreName)
+      .then(function(response) {
+        const books = response.data.items;
+
+        books.forEach(function(book) {
+          const bookCard = createBookCard(book);
+
+          const favoritesButton = bookCard.querySelector('.button.is-primary');
+          favoritesButton.addEventListener('click', function() {
+            favoriteBook(book);
+          });
+
+          bookContainer.appendChild(bookCard);
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  });
+});
+
+function favoriteBook(book) {
+  // Retrieve the existing favorites from local storage
+  const favorites = localStorage.getItem('favorites') || '[]';
+
+  // Parse the favorites into an array
+  const favoritesArray = JSON.parse(favorites);
+
+  // Add the book to the favorites array
+  favoritesArray.push(book);
+
+  // Convert the favorites array back to a string
+  const updatedFavorites = JSON.stringify(favoritesArray);
+
+  // Store the updated favorites in local storage
+  localStorage.setItem('favorites', updatedFavorites);
+}
+
+function createBookCard(book) {
+  const bookCard = document.createElement('div');
+  bookCard.classList.add('box');
+  bookCard.classList.add('has-background-dark');
+  bookCard.style.color = 'white'; // Set text color to white
+
+  const title = document.createElement('h3');
+  title.textContent = book.volumeInfo.title;
+  bookCard.appendChild(title);
+
+  const author = document.createElement('p');
+  author.textContent = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Author not available';
+  bookCard.appendChild(author);
+
+  if (book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail) {
+    const image = document.createElement('img');
+    image.src = book.volumeInfo.imageLinks.thumbnail;
+    image.alt = book.volumeInfo.title;
+    bookCard.appendChild(image);
   }
-  
+
+  const favoritesButton = document.createElement('button');
+  favoritesButton.classList.add('button');
+  favoritesButton.classList.add('is-primary');
+  favoritesButton.textContent = 'Favorite';
+  bookCard.appendChild(favoritesButton);
+
+  // Apply consistent styling to the book card
+  bookCard.style.display = 'flex';
+  bookCard.style.flexDirection = 'column';
+  bookCard.style.alignItems = 'center';
+  bookCard.style.textAlign = 'center';
+
+  title.style.marginTop = '10px';
+  author.style.marginTop = '5px';
+
+  favoritesButton.style.marginTop = 'auto';
+
+  return bookCard;
+}
